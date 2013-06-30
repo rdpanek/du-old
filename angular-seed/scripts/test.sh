35581580d0a88662ea61ea64edf893466a6fe2e0
Radim Daniel Panek <rdpanek@gmail.cz>
2013-06-30 14:37:55 +0200

    Este: remove old source, frontend AngularJs, backend NodeJS

--- angular-seed/scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BASE_DIR=`dirname $0`
-
-echo ""
-echo "Starting Testacular Server (http://vojtajina.github.com/testacular)"
-echo "-------------------------------------------------------------------"
-export NODE_ENV='test';
-testacular start $BASE_DIR/../config/testacular.conf.js
